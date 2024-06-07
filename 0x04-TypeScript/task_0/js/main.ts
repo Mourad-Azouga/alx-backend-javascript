f9ffdf97ed202338715556aa4e45ac017d3b9830
Mourad-Azouga <mourad-azouga@outlook.fr>
2024-06-07 15:50:43 -0700

    Added 0x04-TypeScript, and fixed the Readme for 0x01 lockoxes

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,34 @@
+interface Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+};
+const student1 = createStudent({ firstname: 'john', lastname: "charles", age: 12, location: "Nairobi"})
+const student2 = createStudent({ firstname: 'Donald', lastname: "charles", age: 32, location: "USA"})
+
+const studentsList = [ student1, student2 ];
+
+const table = document.createElement("table");
+const trow = document.createElement("tr");
+const trow1 = document.createElement("tr");
+const tdata = document.createElement("td");
+const tdata1 = document.createElement("td");
+const tdata2 = document.createElement("td");
+const tdata3 = document.createElement("td");
+
+tdata.innerHTML = ;
+tdata1.innerHTML = ;
+
+trow.appendChild(tdata);
+trow.appendChild(tdata1);
+
+table.appendChild(trow);
+
+tdata2.innerHTML = ;
+tdata3.innerHTML = ;
+
+trow1.appendChild(tdata2);
+trow1.appendChild(tdata3);
+
+table.appendChild(trow1);
